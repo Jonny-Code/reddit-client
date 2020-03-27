d312a923ae7758c9ae484f891be62aa26160d53f
Jon Sledge <jonsledge39@gmail.com>
2020-03-27 18:34:15 -0400

    subreddit id

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -14,24 +14,28 @@ export const About: React.FC = () => {
           <h4 className="d-flex align-items-center font-weight-light text-lighter fs-17 m-0">
             139k
           </h4>
-          <h6 className="m-0 pt-1 fs-13 font-weight-light text-lighter">Members</h6>
+          <h6 className="m-0 pt-1 fs-13 font-weight-light text-lighter">
+            Members
+          </h6>
         </div>
         <div className="flex-2 pl-4">
           <h4 className="d-flex align-items-center font-weight-light text-lighter fs-17 m-0">
             348
           </h4>
-          <h6 className="m-0 pt-1 fs-13 font-weight-light text-lighter">Online</h6>
+          <h6 className="m-0 pt-1 fs-13 font-weight-light text-lighter">
+            Online
+          </h6>
         </div>
         <div className="flex-1"></div>
       </div>
-      <hr className="mt-4 mb-2"/>
-      <div className="d-flex">
-      <span className="mr-2">
-      <Cake  className="pt-1"/>
-      </span>
-      <h4 className="d-inline text-lighter fs-14 font-weight-lighter mt-1 mb-2 pb-1 pt-1">
-        Created Jan 25, 2008
-      </h4>
+      <hr className="mt-4 mb-2" />
+      <div className="d-flex align-items-center">
+        <span className="mr-2">
+          <Cake className="pt-1" />
+        </span>
+        <h4 className="d-inline text-lighter fs-14 font-weight-lighter mt-1 mb-2 pb-1 pt-1">
+          Created Jan 25, 2008
+        </h4>
       </div>
     </div>
   );
